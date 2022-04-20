dd504d00fb226a8ade4d10ad533f552a2fdddf11
kemaltt <ttnckemal@gmail.com>
2022-04-20 13:59:40 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const headline = document.getElementById('headline')
+    // console.log(headline);
+const firstColor = () => {
+    document.body.style.backgroundColor = '#1D8F52'
+    headline.style.color = '#6E0960'
+}
+const secondColor = () => {
+    document.body.style.backgroundColor = ' #454271'
+    headline.style.color = '#DAAB42'
+}
+const thirdColor = () => {
+    document.body.style.backgroundColor = ' #00011F '
+    headline.style.color = '#C65D0B'
+}
+const fourthColor = () => {
+    document.body.style.backgroundColor = ' #050854 '
+    headline.style.color = '#29CCFE '
+}
